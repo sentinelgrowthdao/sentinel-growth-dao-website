881db52fda025e08c02c881f2f022cf9d0dae5f8
trinityprivacy <trimorneo@proton.me>
2024-01-25 22:03:46 +1100

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/dao-social-card.png',
       navbar: {
         title: 'Sentinel Growth DAO',
         logo: {
